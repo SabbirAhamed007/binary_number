a5c597995a2075e51777e9b84ff013b99a1ca35a
Sabbir <54003568+SabbirAhamed007@users.noreply.github.com>
2020-10-02 09:54:32 -0400

    Initial commit

---
+++ binary_converter.py
@@ -0,0 +1,195 @@
+v = input("Enter your password to see it's binary number: ")
+
+for str in v:
+    if str == "A":
+        print("01000001")
+    print(str)
+    if str == "B":
+        print("01000010")
+    if str == "C":
+        print("01000011")
+    if str == "D":
+        print("01000100")
+    if str == "E":
+        print("01000101")
+    if str == "F":
+        print("01000110")
+    if str == "G":
+        print("01000111")
+    if str == "H":
+        print("	01001000")
+    if str == "I":
+        print("01001001")
+    if str == "J":
+        print("01001010")
+    if str == "K":
+        print("01001011")
+    if str == "L":
+        print("01001100")
+    if str == "M":
+        print("01001101")
+    if str == "N":
+        print("01001110")
+    if str == "O":
+        print("01001111")
+    if str == "P":
+        print("01010000")
+    if str == "Q":
+        print("01010001")
+    if str == "R":
+        print("01010010")
+    if str == "S":
+        print("01010011")
+    if str == "T":
+        print("01010100")
+    if str == "U":
+        print("01010101")
+    if str == "V":
+        print("01010110")
+    if str == "W":
+        print("01010111")
+    if str == "X":
+        print("01011000")
+    if str == "Y":
+        print("01011001")
+    if str == "Z":
+        print("01011010")
+
+    if str == "a":
+         print("01100001")
+    if str == "b":
+         print("01100010")
+    if str == "c":
+         print("01100011")
+    if str == "d":
+         print("01100100")
+    if str == "e":
+         print("01100101")
+    if str == "f":
+         print("01100110")
+    if str == "g":
+         print("01100111")
+    if str == "h":
+         print("01101000")
+    if str == "i":
+         print("01101001")
+    if str == "j":
+        print("01101010")
+    if str == "k":
+        print("01101011")
+    if str == "l":
+        print("01101100")
+    if str == "m":
+        print("01101101")
+    if str == "n":
+        print("01101110")
+    if str == "o":
+        print("01101111")
+    if str == "p":
+         print("01110000")
+    if str == "q":
+         print("01110001")
+    if str == "r":
+        print("01110010")
+    if str == "s":
+        print("01110011")
+    if str == "t":
+        print("01110100")
+    if str == "u":
+        print("01110101")
+    if str == "v":
+        print("01110110")
+    if str == "w":
+         print("01110111")
+    if str == "x":
+        print("01111000")
+    if str == "y":
+        print("01111001")
+    if str == "z":
+        print("01111010")
+
+    if str == "0":
+         print("0")
+    if str == "1":
+         print("1")
+    if str == "2":
+         print("10")
+    if str == "3":
+         print("11")
+    if str == "4":
+         print("100")
+    if str == "5":
+         print("101")
+    if str == "6":
+         print("110")
+    if str == "7":
+         print("111")
+    if str == "8":
+         print("1000")
+    if str == "9":
+         print("1001")
+
+    if str == " ":
+         print("100000")
+    if str == "!":
+         print("100001")
+    if str == "'":
+         print("100010")
+    if str == "#":
+         print("100011")
+    if str == "$":
+         print("100100")
+    if str == "%":
+         print("100101")
+    if str == "&":
+         print("100110")
+    if str == "'":
+         print("100111")
+    if str == "(":
+         print("101000")
+    if str == ")":
+         print("101001")
+    if str == "*":
+         print("101010")
+    if str == "+":
+         print("101011")
+    if str == ",":
+         print("101100")
+    if str == "-":
+         print("101101")
+    if str == ".":
+         print("101110")
+    if str == "/":
+         print("101111")
+    if str == ":":
+         print("111010")
+    if str == ";":
+         print("111011")
+    if str == "<":
+         print("111100")
+    if str == "=":
+         print("111101")
+    if str == ">":
+         print("111110")
+    if str == "?":
+         print("111111")
+    if str == "@":
+         print("1000000")
+    if str == "[":
+         print("1011011")
+    if str == "]":
+         print("1011101")
+    if str == "^":
+         print("1011110")
+    if str == "_":
+         print("1011111")
+    if str == "`":
+         print("1100000")
+    if str == "{":
+         print("1111011")
+    if str == "|":
+         print("1111100")
+    if str == "}":
+         print("1111101")
+    if str == "~":
+         print("1111110")
